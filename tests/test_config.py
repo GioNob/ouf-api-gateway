3ccc4d11bfa3432957c66b1f87acbed9669892e1
GioNob <96419716+GioNob@users.noreply.github.com>
2026-09-24 13:40:37 +0200

    test: include governed UDP policy workload

--- tests/test_config.py
+++ tests/test_config.py
@@ -120,5 +120,6 @@ def test_policy_bundle_route_is_real_authorization_capability():
     assert route["x-ouf-policy"]["allowedServiceIdentities"]==[
         "installation://iam.workloadClients.mcpServer",
         "installation://iam.workloadClients.ingestion",
+        "installation://iam.workloadClients.udp",
     ]
     assert route["x-ouf-policy"]["allowedActorTypes"]==["SERVICE"]
